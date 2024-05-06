ad78efbffb47ec799a13d96cc5c5c15068ff694f
DanielEstebanChaconDiaz <138712462+DanielEStebanChaconDiaz@users.noreply.github.com>
2024-05-06 13:02:47 -0500

    feat: :construction: Creacion de modulos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-import {
-    getAllMissions
-} from './module/rockets.js'
-console.log(await getAllMissions())
+import { getAllRocketEngineTotal } from "./modules/rockets.js";
+import { 
+    paginationRockets
+} from "./modulesComponents/pagination.js";
+
+
+let paginacion = document.querySelector("#paginacion");
+paginacion.innerHTML = ""
+paginacion.append(await paginationRockets())
+
